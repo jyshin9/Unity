188af1c8c1619d6d823c57de283699676ee18f53
Juyoung Shin <baekju092@gmail.com>
2023-09-22 15:30:13 +0900

    Commit

---
+++ FlappyClone/Assets/Fixed.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fixed : MonoBehaviour
+{
+    void Start(){
+        Screen.SetResolution(1080, 1920, true);
+    }
+    void Awake(){
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(1080, 1920, true);
+    }
+}
