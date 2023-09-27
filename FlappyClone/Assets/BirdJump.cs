17528c0a063808abf1640318edd68f743ef60aea
Juyoung Shin <baekju092@gmail.com>
2023-09-27 11:20:03 +0900

    Commit

--- FlappyClone/Assets/BirdJump.cs
+++ FlappyClone/Assets/BirdJump.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement;
 public class BirdJump : MonoBehaviour //MonoBehaviour를 상속받음
 {
     Rigidbody2D rb; //Rigidbody2D(클래스)를 감당할 수 있는(?) rb라는 박스 생성
-    public float jumpPower; 
+    public float jumpPower;
     public float SuperTime;
+    public Move moveScript; //Move 스크립트의 참조를 저장할 변수
 
     // Start is called before the first frame update 최초 한번
     void Start()
@@ -25,24 +26,54 @@ public class BirdJump : MonoBehaviour //MonoBehaviour를 상속받음
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private bool isFrozen = false;
+
+    public void OnCollisionEnter2D(Collision2D other)
     { //collision끼리 부딪혔을 때
-        if(other.gameObject.tag == "obstacle"){
+        if (other.gameObject.tag == "obstacle")
+        {
             if (Score.score > Score.bestScore)
-            { 
+            {
                 Score.bestScore = Score.score;
             }
             SceneManager.LoadScene("GameOverScene"); //GameOverScene으로 넘어감
         }
-        if(other.gameObject.tag == "Apple"){ //apple 먹었을 시
+        if (other.gameObject.tag == "Apple")
+        { //apple 먹었을 시
+            if (!isFrozen) //중력고정
+            {
+                StartCoroutine(FreezeObject());
+            }
             StartCoroutine(SuperMode()); //무적모드
             Destroy(other.gameObject); //아이템 삭제
             //이동 속도 증가
-            //중력가속도 없애기
+            moveScript.speed = 10.0f;
         }
     }
 
-    IEnumerator SuperMode(){ //StartCoroutine의 본체
+    public IEnumerator FreezeObject()
+    {
+        isFrozen = true;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+
+        //일정 시간 동안 고정
+        yield return new WaitForSeconds(SuperTime);
+
+        //고정 해제
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+        isFrozen = false;
+    }
+
+    IEnumerator SuperMode()
+    { //StartCoroutine의 본체
         //this는 이 BirdJump 스크립트를 가지고 있는 오브젝트 == Bird를 뜻함.
         this.gameObject.layer = 7; //super layer로 변경
         GetComponent<SpriteRenderer>().color = Color.yellow;
--- FlappyClone/Assets/Move.cs
+++ FlappyClone/Assets/Move.cs
@@ -15,10 +15,6 @@ public class Move : MonoBehaviour
     void Update()
     {
         //(-1,0,0) 매시간마다 x축 방향으로 -1씩 이동, Time.deltaTime을 이용하여 fps보정
-        transform.position += Vector3.left * test(speed) * Time.deltaTime;
-    }
-
-    int test(test){
-        return test
+        transform.position += Vector3.left * speed * Time.deltaTime;
     }
 }
